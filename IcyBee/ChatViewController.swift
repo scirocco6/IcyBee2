4730ea80e90d5adf9e9c2f03f41393759e64d258
six <scirocco6@gmail.com>
2016-11-23 11:31:17 -0800

    scaffolding for textField keyboard will show and hide delegate and message registration

--- IcyBee/ChatViewController.swift
+++ IcyBee/ChatViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var titleBar: UINavigationItem?
     @IBOutlet var textView: UITextView?
+    @IBOutlet var inputLine: UITextField?
     
     let emptyString  = NSMutableAttributedString(string: "")
     let spaceString  = NSMutableAttributedString(string: " ")
@@ -20,7 +21,6 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
 
         // subscribe to topic change messages
         NotificationCenter.default.addObserver(self,
@@ -32,6 +32,24 @@ class ChatViewController: UIViewController {
                                                selector: #selector(ChatViewController.addMessage(_:)),
                                                name: NSNotification.Name(rawValue: "FNNewMessage"),
                                                object: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(ChatViewController.keyboardWillShow(_:)),
+                                               name: NSNotification.Name(rawValue: "UIKeyboardWillShowNotification"),
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(ChatViewController.keyboardWillHide(_:)),
+                                               name: NSNotification.Name(rawValue: "UIKeyboardWillHideNotification"),
+                                               object: nil)
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: "UIKeyboardWillShowNotification"), object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: "UIKeyboardWillHideNotification"), object: nil)
     }
     
     override func didReceiveMemoryWarning() {
@@ -62,5 +80,43 @@ class ChatViewController: UIViewController {
         let bottom = NSMakeRange((textView?.text.characters.count)! - 1, 1)
         textView?.scrollRangeToVisible(bottom)
     }
+    
+// Mark - UITextFieldDelegate
+    func keyboardWillShow(_ notification: Notification) {
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+    }
+    
+//    - (void)keyboardWillShow:(NSNotification *) notification {
+//    CGSize keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+//    
+//    CGRect aRect = self.view.frame;
+//    
+//    aRect.size.height -= keyboardSize.height;
+//    if (!CGRectContainsPoint(aRect, inputTextField.frame.origin) ) {
+//    UIEdgeInsets contentInsets = UIEdgeInsetsMake(scrollView.contentInset.top, 0.0, keyboardSize.height, 0.0);
+//    scrollView.contentInset = contentInsets;
+//    scrollView.scrollIndicatorInsets = contentInsets;
+//    }
+//    }
+//    
+//    
+//    - (void) keyboardWillHide:(NSNotification *) notification {
+//    UIEdgeInsets contentInsets = UIEdgeInsetsMake(scrollView.contentInset.top, 0.0, 0.0, 0.0);
+//    scrollView.contentInset = contentInsets;
+//    scrollView.scrollIndicatorInsets = contentInsets;
+//    }
+//    
+//    -(BOOL)textFieldShouldReturn:(UITextField *)textField {
+//    [inputTextField resignFirstResponder];
+//    
+//    if([[inputTextField text] length]) {
+//    [[IcbConnection sharedInstance] processInput: [inputTextField text]];
+//    [inputTextField setText:@""];
+//    }
+//    
+//    return YES;
+//    }
 }
 
