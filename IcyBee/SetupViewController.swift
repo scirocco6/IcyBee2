3c2b08ae38c14d2712a2f476867dda2897667f23
six <six@obsoletetechnology.org>
2017-07-12 17:06:37 -0400

    move between fields with next button collapse keyboard with done button
    if all fields not filled out when JOIN pressed show those fields in red
    show errors in red
    error messages for fields not filled out and password and confirm mismatch

--- IcyBee/SetupViewController.swift
+++ IcyBee/SetupViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 import IcbKit
 
-class SetupViewController: UIViewController {
+class SetupViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var nickname: UITextField!
+    @IBOutlet var channel:  UITextField!
+    @IBOutlet var password: UITextField!
+    @IBOutlet var confirm:  UITextField!
+    
+    @IBOutlet var problems: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         if (UserDefaults.standard.string(forKey: "nick_preferences") != nil) {
@@ -24,7 +31,58 @@ class SetupViewController: UIViewController {
     
 
     // MARK: - Navigation
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField.returnKeyType == .next) {
+            let nextField = textField.superview?.viewWithTag(textField.tag + 1) as! UITextField
+            // nextField.becomeFirstResponder  // this no longer works in iOS 8+
+            nextField.perform(#selector(becomeFirstResponder), with: nil, afterDelay: 0.0)
+        }
+        else if (textField.returnKeyType == .done) {
+            // textField.resignFirstResponder // double ugh, see above
+            textField.perform(#selector(resignFirstResponder), with: nil, afterDelay: 0.0)
+        }
 
+        return true
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if (identifier == "joinIcb") {return processSetupDetails()}
+        return true
+    }
+    
+    func processSetupDetails() -> Bool {
+        var errorString = ""
+        var setupComplete = true
+        
+        if (nickname.text == "" || channel.text == "" || password.text == "" || confirm.text == "") {
+            let placeholderString = NSAttributedString(string: "required", attributes: [NSForegroundColorAttributeName: UIColor.red])
+            
+            nickname.attributedPlaceholder = placeholderString
+            channel.attributedPlaceholder  = placeholderString
+            password.attributedPlaceholder = placeholderString
+            confirm.attributedPlaceholder  = placeholderString
+            
+            errorString = "Please fill in all required fields."
+            setupComplete = false
+        }
+
+        if password.text?.isEmpty == false && confirm.text?.isEmpty == false && password.text != confirm.text {
+            if errorString != "" {
+                errorString += "\n"
+            }
+            
+            errorString += "The password and confirmation fields do not match."
+            setupComplete = false
+        }
+        
+        if setupComplete == false {
+            problems.text = errorString
+            problems.isHidden = false
+        }
+        
+        return setupComplete
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         IcbDelegate.icbController.connect()
     }
